5554e30bfdd6ff7bdb1a5b8bb9ed056a2abcd47b
vivid20221 <julainj9011@gmail.com>
2022-09-23 01:18:00 -0500

    js var

--- develope/assets/script.js
+++ develope/assets/script.js
@@ -2,5 +2,27 @@
 //hit the submit 
 
 var
-var
-var
+var timerElement = document.querySelector(".timer_display");
+var startButton =document.querySelector(".start_button");
+
+
+// user clicks start quiz
+
+
+
+//timer starts at 75
+
+
+// 5 quesuions with 4 answers when selected a answer the user will get a display of wrong! or correct drop down
+
+
+
+//  when answered wrong user gets 17 secs off the clock
+
+
+
+// user gets a all done display and input intals with submit button
+//displays final score  "your final score"
+
+
+// Then user gets the highscores screen and displays intal-22 then user choice button go back or Clear Highscores 

74ec76b89051f10fcb1a85111965d17cf36ea550
vivid20221 <julainj9011@gmail.com>
2022-09-26 17:54:39 -0500

    modified

--- develope/assets/script.js
+++ develope/assets/script.js
@@ -1,7 +1,7 @@
 // timers intervals 9  arry , setting qu as objects 
 //hit the submit 
 
-var
+var quizContainer = document.querySelector("quiz");
 var timerElement = document.querySelector(".timer_display");
 var startButton =document.querySelector(".start_button");
 
@@ -12,17 +12,28 @@ var startButton =document.querySelector(".start_button");
 
 //timer starts at 75
 
+timerElement 
 
-// 5 quesuions with 4 answers when selected a answer the user will get a display of wrong! or correct drop down
+// 5 quesuions with 4 answers when selected a answer the user will get a display of wrong! or correct drop down /
+
+{1. In terms of DOM, <head> =  Choose matching definition A. document.documentElement B. elem.firstChild C. True or False D. document.head
+
+2. What will an undeclared variable return? Choose matching definition A. A letter, $ or _ B. Undefined C. True or False D. Within a function, only available within function
+
+3. .addEventListener Choose matching definition A. ('click' ( ) {p.textContent = input.value }) B. using the var keyword - eg. var myName="Monica"; C. similar to .innerHTML D. The DOM is a representation of a webpage that JavaScript can use.
+
+4. What are the two ways to add JS to your site? Choose matching definition A. A letter, $ or _ B. True or False C .Outside a function, available to any code outside that function (also within). D. Internally and Externally.
 
 
 
-//  when answered wrong user gets 17 secs off the clock
+//*when answered wrong user gets 17 secs off the clock //
 
 
 
-// user gets a all done display and input intals with submit button
+
+
+// user gets a all done display and input intals with submit button/
 //displays final score  "your final score"
 
 
-// Then user gets the highscores screen and displays intal-22 then user choice button go back or Clear Highscores 
+// Then user gets the highscores screen and displays intal-22 then user choice button go back or Clear Highscores /
